8f1989b5c640edf85c2ce4da106fcd658b4457b1
nonom <yoni.mey11@gmail.com>
2022-03-08 20:05:52 +0200

    Bonus

--- HoleFillingCmbine/src/main/java/MainClass.java
+++ HoleFillingCmbine/src/main/java/MainClass.java
@@ -6,17 +6,24 @@ public class MainClass {
     public static void main(String[] args) {
         ImageFixing.BoundaryType type = args[5] == "8" ? ImageFixing.BoundaryType.EIGHT : ImageFixing.BoundaryType.FOUR;
         System.out.println(args[6]);
-        Boolean mode = args[6].equals("original") ? true : false;
+        int mode = Integer.valueOf(args[6]);
         System.out.println(args[0]);
         BufferedImage img = ImageIO.readImg(args[0]);
         BufferedImage mask = ImageIO.readImg(args[1]);
         BufferedImage margeImg = ImagePrepare.maskMerge(img,mask);
         BufferedImage greyImg = ImagePrepare.toGreyScale(margeImg);
         float[][] floatImg = ImagePrepare.toFloatArray(greyImg);
-        if(mode)
-            ImageFixing.fixHole2(floatImg, type, Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-        else
-            ImageFixing.fixHole(floatImg, type, Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+        switch (mode){
+            case 1:
+                ImageFixing.fixHole2(floatImg, type, Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+                break;
+            case 2:
+                ImageFixing.fixHole(floatImg, type, Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+                break;
+            case 3:
+                ImageFixing.fixHole3(floatImg, type);
+                break;
+        }
         greyImg = ImagePrepare.toBufferedImage(floatImg);
         ImageIO.writeImg(greyImg, args[2]);
     }
